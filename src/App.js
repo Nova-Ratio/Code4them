4f12be4a91b8f0bfa0c0645203a67b2233ebd716
Nova Ratio <novaratio.tech@gmail.com>
2023-02-09 20:34:57 +0300

    Update index.html and app.js

--- src/App.js
+++ src/App.js
@@ -30,39 +30,35 @@ function App() {
       <header className="flex w-full justify-center font-bold">
         <h1 className="text-[#FCFBF7] text-4xl">#Code4Them</h1>
       </header>
-      <footer className="flex w-full justify-center">
-        <span className="text-[#FCFBF7] text-lg text-center">
+      <main className="flex-1 w-full flex flex-col items-center gap-20 overflow-auto scrollbar-hidden">
+        <p className="text-[#FCFBF7] md:text-base sm:text-sm text-xs text-center">
           Thank you to everyone who has helped so far. Both moral and financial
           support are very important during these critical times. But the true
           face of this event has not yet been revealed. We will realize the real
           problems when we remember that the survivors must survive. This
           disaster destroyed thousands of dreams, but our support must be
           continuous, not instant, so that the dreams of millions who survived
-          it do not disappear. As developers, we have to do what we can. We beg
-          all developers in the ecosystem. We are not asking you to donate
+          it do not disappear. As developers, we have to do what we can. We beg to
+          all developers in the world. We are not asking you to donate
           money, we are not asking you to send items, we are just asking you to
           do what you do best. This time don't code for money, don't code for
-          reputation. Code for the millions in the cold, code for the childeren
-          in the dark, code for the surviviors in despair, code with us, code
+          reputation. Code for the millions in the cold, code for the children
+          in the dark, code for the survivors in despair, code with us, code
           for them. Just make a project, it doesn't have to be groundbreaking,
           it doesn't have to be a world-changing project because they don't have
           a world anymore, just help them survive.
-        </span>
-      </footer>
-      <img src={"/image/black.png"} />
-      <main className="flex-1 w-full flex flex-col items-center gap-8 overflow-auto scrollbar-hidden">
-        {projects.map((project, idx) => (
-          <ProjectCard key={idx} project={project} />
-        ))}
+        </p>
+        <img alt="black-ribbon" src={"/image/black.png"} className="max-w-[220px] w-full" />
+        <div className="w-full flex flex-col items-center gap-8">
+          {projects.map((project, idx) => (
+            <ProjectCard key={idx} project={project} />
+          ))}
+        </div>
       </main>
       <footer className="flex w-full justify-center">
         <span className="text-[#FCFBF7] text-lg text-center">
           To list your project, please open pull request from github.{" "}
-          <a
-            href="https://github.com/Nova-Ratio/Code4them"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://github.com/Nova-Ratio/Code4them" target="_blank" rel="noreferrer">
             Github Link Here
           </a>
         </span>
